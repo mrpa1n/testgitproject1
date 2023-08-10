54e7f8e7dda3702f39ee87110bd906cc408f34ce
mrpa1n <140310761+mrpa1n@users.noreply.github.com>
2023-08-10 14:30:24 +0530

    Update String_methods.java

--- String_methods.java
+++ String_methods.java
@@ -4,17 +4,17 @@ public class String_methods {
 
 	public static void main(String[] args) {
 		
-		String name ="My name is Pratham";
-		String abc = "MY NAME IS PRATHAM";
-		String xyz = "  My name is Pratham  ";
+		String name ="My name is omkar";
+		String abc = "MY NAME IS JAISWAR";
+		String xyz = "  My name is Omkar  ";
 		
 		System.out.println(name.length());
 		
 		System.out.println(name.charAt(3));
 		
-		System.out.println(name.equals("My name is Pratham"));
+		System.out.println(name.equals("My name is Omkar"));
 		
-		System.out.println(name.equalsIgnoreCase("MY Name IS Pratham"));
+		System.out.println(name.equalsIgnoreCase("MY Name IS omkar"));
 		
 		System.out.println(name.concat(abc));
 		
